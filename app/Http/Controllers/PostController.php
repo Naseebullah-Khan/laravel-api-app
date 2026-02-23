fe13209f477f0a771fc347d102f0d4e8c31f18a7
Naseeb Khan <110621136+Naseebullah-Khan@users.noreply.github.com>
2026-02-24 00:45:13 +0430

    Completed the CRUD Functionality

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,46 +5,56 @@ namespace App\Http\Controllers;
 use App\Http\Requests\StorePostRequest;
 use App\Http\Requests\UpdatePostRequest;
 use App\Models\Post;
+use Illuminate\Support\Collection;
 
 class PostController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Collection
     {
-        //
+        return Post::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(StorePostRequest $request)
+    public function store(StorePostRequest $request): Post
     {
-        //
+        $newPost = new Post();
+        $newPost->title = $request->title;
+        $newPost->body = $request->body;
+        $newPost->save();
+
+        return $newPost;
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Post $post)
+    public function show(Post $post): Post
     {
-        //
+        return $post;
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(UpdatePostRequest $request, Post $post)
+    public function update(UpdatePostRequest $request, Post $post): Post
     {
-        //
+        $post->update(attributes: $request->all());
+
+        return $post;
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Post $post)
+    public function destroy(Post $post): array
     {
-        //
+        $post->delete();
+
+        return ["message" => "The post was deleted"];
     }
 }
